86a14d7fadbb1e7821abea26c0aed1e4c0fcf261
Connor Franklin <connor.franklin@ogilvy.com>
2014-03-26 22:28:03 -0400

    revised add function, added delete, api info now persisted as Object and saved to disk

--- okapi.js
+++ okapi.js
@@ -1,10 +1,64 @@
 var Hapi = require('hapi');
 var Handlebars = require('handlebars');
-var Fs = require('fs');
+var Fs = require('fs-extra');
 
-console.log('hellokapi');
+
+var Storage = {
+    newAPI: function(obj) {
+        console.log(Storage.storage, obj, Storage.storage.api)
+        Storage.storage.api[obj.name] = obj;
+        Storage.store();
+        console.log('NEW API')
+    },
+    deleteAPI: function(name) {
+        if (Storage.storage.api.hasOwnProperty(name)) {
+            delete Storage.storage.api[name];
+            Storage.store();
+            Fs.remove(__dirname + '/data/' + name, function(err) {
+                if (err) {
+                    return console.error(err)
+                };
+                console.log('DELETED FILE')
+            });
+            console.log('DELETED API: ' + name)
+        }
+    },
+    store: function() {
+        Fs.writeFile(__dirname + '/data/data.json', JSON.stringify(Storage.storage), 'utf8', function() {
+            console.log('STORED TO DISK');
+        });
+    }
+}
+//LOAD STATUS FROM DISK
+Fs.exists(__dirname + '/data', function(does) {
+    if (!does) {
+        Fs.mkdir(__dirname + '/data');
+        initAndStore();
+    }
+
+    Fs.exists(__dirname + '/data/data.json', function(does) {
+        if (does) {
+            Fs.readFile(__dirname + '/data/data.json', 'utf8', function(err, data) {
+                Storage.storage = JSON.parse(data);
+                console.log('STORAGE.STORAGE POPULATED FROM DATA.JSON');
+            });
+        } else {
+            initAndStore();
+        }
+    });
+
+    function initAndStore() {
+        Storage.storage = {
+            'api': {}
+        };
+        Storage.store();
+        console.log('STORAGE FILE DATA.JSON CREATED')
+    }
+});
 
 var server = Hapi.createServer('0.0.0.0', +process.env.PORT || 3000);
+
+console.log('hellokapi');
 
 server.views({
     engines: {
@@ -24,34 +78,39 @@ controller.api.keyValue = {
             key = req.params.key,
             value = req.params.value;
 
-        console.log(api, '{ ' + key + ': \'' + value + '\' }');
+        if (Storage.storage.api[api]) {
 
-        Fs.readFile('./data/' + api + '/' + api + '.json', 'utf8', function(err, data) {
-            if (err) {
-                console.log('Error: ' + err);
-                return;
-            }
-            data = JSON.parse(data);
-            var results = {
-                meta: {
-                    name: api,
-                    source: 'OKAPI',
-                    query: {},
-                    totalResults: 0
-                },
-                results: [],
-            }
+            console.log(api, '{ ' + key + ': \'' + value + '\' }');
 
-            results.meta.query[key] = value;
-
-            for (i = 0; i < data.csvRows.length; i++) {
-                if (data.csvRows[i][key] === value) {
-                    results.results.push(data.csvRows[i]);
-                    results.meta.totalResults += 1;
+            Fs.readFile(__dirname + '/data/' + api + '/' + api + '.json', 'utf8', function(err, data) {
+                if (err) {
+                    console.log('ERROR: ' + err);
+                    return;
                 }
-            }
-            reply(results);
-        });
+                data = JSON.parse(data);
+                var results = {
+                    meta: {
+                        name: api,
+                        source: 'GSA.gov Generic API',
+                        query: {},
+                        totalResults: 0
+                    },
+                    results: [],
+                }
+
+                results.meta.query[key] = value;
+
+                for (i = 0; i < data.csvRows.length; i++) {
+                    if (data.csvRows[i][key] === value) {
+                        results.results.push(data.csvRows[i]);
+                        results.meta.totalResults += 1;
+                    }
+                }
+                reply(results);
+            });
+        } else {
+            reply('<h1>GSA Generic API</h1><h2>API Not Found</h2>')
+        }
     }
 }
 
@@ -61,124 +120,143 @@ controller.api.queryString = {
         var api = req.params.api,
             query = req.query;
 
-        if (Object.size(query) === 0) {
-            reply('<h1>OKAPI</h1><h2>' + api + '</h2>');
-        } else {
+        if (Storage.storage.api[api]) {
+            if (Object.size(query) === 0) {
+                reply('<h1>GSA Generic API</h1><h2>' + Storage.storage.api[api].displayName + '</h2><ul><li><a href="./' + api + '/delete">Delete this API</a></li></ul>');
+            } else {
 
-            console.log(api, query);
+                console.log(api, query);
 
-            Fs.readFile('./data/' + api + '/' + api + '.json', 'utf8', function(err, data) {
-                if (err) {
-                    console.log('Error: ' + err);
-                    return;
-                }
-                data = JSON.parse(data);
-                var results = {
-                    meta: {
-                        name: api,
-                        source: 'OKAPI',
-                        query: query,
-                        totalResults: 0
-                    },
-                    results: [],
-                }
-                //FIND IN ARRAY OF OBJECTS
-                Array.prototype.find = function(obj) {
+                Fs.readFile(__dirname + '/data/' + api + '/' + api + '.json', 'utf8', function(err, data) {
+                    if (err) {
+                        console.log('Error: ' + err);
+                        return;
+                    }
+                    data = JSON.parse(data);
+                    var results = {
+                        meta: {
+                            name: api,
+                            source: 'GSA.gov Generic API',
+                            query: query,
+                            totalResults: 0
+                        },
+                        results: [],
+                    }
+                    //FIND IN ARRAY OF OBJECTS
+                    Array.prototype.find = function(obj) {
 
-                    for (var i = 0, len = this.length; i < len; i++) {
-                        var ele = this[i],
-                            match = true;
+                        for (var i = 0, len = this.length; i < len; i++) {
+                            var ele = this[i],
+                                match = true;
 
-                        //SEE IF KEY CONTAINS WILDCARD
-                        for (var x in obj) {
-                            if (obj[x].toString().substr(obj[x].length - 1) === '*') {
-                                //WILDCARD AT BEGINNING AND END (CONTAINS)
-                                if (obj[x].toString().substr(0, 1) === '*') {
-                                    //IF SO, REMOVE WILDCARDS
-                                    var newKey = obj[x].replace(/\*/g, '');
-                                    //BUILD REGEX
-                                    var regex = new RegExp(newKey);
-                                    //MATCH AGAINST REGEX
-                                    if (ele[x].match(regex) < 1) {
+                            //SEE IF KEY CONTAINS WILDCARD
+                            for (var x in obj) {
+                                if (obj[x].toString().substr(obj[x].length - 1) === '*') {
+                                    //WILDCARD AT BEGINNING AND END (CONTAINS)
+                                    if (obj[x].toString().substr(0, 1) === '*') {
+                                        //IF SO, REMOVE WILDCARDS
+                                        var newKey = obj[x].replace(/\*/g, '');
+                                        //BUILD REGEX
+                                        var regex = new RegExp(newKey);
+                                        //MATCH AGAINST REGEX
+                                        if (ele[x].match(regex) < 1) {
+                                            match = false;
+                                            break;
+                                        }
+                                    }
+                                    //WILDCARD AT END ONLY
+                                    else {
+                                        var newKey = obj[x].replace('*', '');
+                                        var regex = new RegExp('^' + newKey);
+                                        if (ele[x].match(regex) < 1) {
+                                            match = false;
+                                            break;
+                                        }
+                                    }
+                                } else {
+                                    if (ele[x] !== obj[x]) {
                                         match = false;
                                         break;
                                     }
-                                }
-                                //WILDCARD AT END ONLY
-                                else {
-                                    var newKey = obj[x].replace('*', '');
-                                    var regex = new RegExp('^' + newKey);
-                                    if (ele[x].match(regex) < 1) {
-                                        match = false;
-                                        break;
-                                    }
-                                }
-                            } else {
-                                if (ele[x] !== obj[x]) {
-                                    match = false;
-                                    break;
                                 }
                             }
+
+                            if (match) {
+                                results.results.push(data.csvRows[i]);
+                                results.meta.totalResults += 1;
+                            }
                         }
+                    };
 
-                        if (match) {
-                            results.results.push(data.csvRows[i]);
-                            results.meta.totalResults += 1;
-                        }
-                    }
-                };
+                    data.csvRows.find(query);
 
-                data.csvRows.find(query);
-
-                reply(results);
-            });
+                    reply(results);
+                });
+            }
+        } else {
+            reply('<h1>GSA Generic API</h1><h2>API Not Found</h2>')
         }
     }
 }
 
-server.route({
-    method: 'GET',
-    path: '/',
+controller.api.baleted = {
     handler: function(req, reply) {
-        reply('<h1>OKAPI</h1><ul><li><a href="/api">See Available APIs</a></li><li><a href="/upload">Upload a .CSV</a></li>');
+        var name = req.params.api;
+        Storage.deleteAPI(name);
+        reply('Deleted API: ' + name + metaRefresh(3, '/'));
     }
-});
+}
 
 server.route({
     method: 'POST',
-    path: '/uploader',
+    path: '/upload',
     config: {
         payload: {
             output: 'stream',
             parse: true
         },
         handler: function(req, reply) {
-            var fileName = req.payload.name.toLowerCase().replace(/ /g, '-');
-            Fs.mkdir('./data/' + fileName)
-            req.payload.file /*.pipe(Zlib.createGunzip())*/ .pipe(Fs.createWriteStream('./data/' + fileName + '/' + fileName + '.csv'));
-            console.log(fileName);
+            var displayName = req.payload.name;
+            var name = displayName.toLowerCase().replace(/ /g, '-');
 
-            reply('<h1>OKAPI</h1><p>Your new API Lives <a href="/api/' + fileName + '">Here</a>.</p>');
+            if (Storage.storage.api[name]) {
+                reply('An API with this name already exists.');
+            } else {
+                Fs.mkdir(__dirname + '/data/' + name)
 
-            var Converter = require("csvtojson").core.Converter;
+                var csvFile = Fs.createWriteStream(__dirname + '/data/' + name + '/' + name + '.csv');
 
-            var csvFileName = './data/' + fileName + '/' + fileName + '.csv';
+                req.payload.file /*.pipe(Zlib.createGunzip())*/ .pipe(csvFile);
 
-            var csvConverter = new Converter();
+                csvFile.on('finish', function() {
+                    console.log('SAVED AS CSV');
 
-            csvConverter.on("end_parsed", function(jsonObj) {
-                console.log('CONVERTED TO JSON')
-                jsonObj = JSON.stringify(jsonObj);
-                Fs.writeFile('./data/' + fileName + '/' + fileName + '.json', jsonObj, function(err) {
-                    if (err) {
-                        console.log(err);
-                    } else {
-                        console.log('SAVED AS JSON');
-                    }
+                    var Converter = require("csvtojson").core.Converter;
+
+                    var csvname = __dirname + '/data/' + name + '/' + name + '.csv';
+
+                    var csvConverter = new Converter();
+                    console.log('CONVERTING...');
+                    csvConverter.from(csvname);
+
+                    csvConverter.on("end_parsed", function(json) {
+                        console.log('CONVERTED TO JSON');
+                        json = JSON.stringify(json);
+                        Fs.writeFile(__dirname + '/data/' + name + '/' + name + '.json', json, function(err) {
+                            if (err) {
+                                console.log(err);
+                            } else {
+                                console.log('SAVED AS JSON');
+                                Storage.newAPI({
+                                    name: name,
+                                    displayName: displayName
+                                });
+                                reply('<h1>GSA Generic API</h1><p>Your new API Lives Here: <a href="/api/' + name + '">' + displayName + '</a>.</p><ul><li><a href="/add">Add Another .CSV</a></li></ul');
+                            }
+                        });
+                    });
                 });
-            });
-
-            csvConverter.from(csvFileName);
+            }
         }
     }
 });
@@ -194,27 +272,23 @@ server.route({
     method: 'GET',
     config: controller.api.queryString
 });
+
 server.route({
-    path: '/api',
+    path: '/api/{api}/delete',
+    method: 'GET',
+    config: controller.api.baleted
+});
+
+server.route({
+    path: '/',
     method: 'GET',
     handler: function(req, reply) {
-        var apiName = [];
-        Fs.readdir('./data', function(err, files) {
-            if (err) throw err;
-            files.forEach(function(file) {
-                if (file.indexOf('.') === -1) {
-                    apiName.push({
-                        name: file
-                    });
-                }
-            });
-        });
-
-        if (apiName.length === -1) {
-            reply('<h1>OKAPI</h1><h2>No APIs Available</h2>')
+        var api = Storage.storage.api;
+        if (api.length < 1) {
+            reply('<h1>GSA Generic API</h1><h2>No APIs Available</h2>')
         } else {
             reply.view('api-index.html', {
-                api: apiName
+                api: api
             });
         }
     }
@@ -222,18 +296,17 @@ server.route({
 
 server.route({
     method: 'GET',
-    path: '/upload',
+    path: '/add',
     handler: function(req, reply) {
-        reply('<h1>OKAPI</h1><p>Upload a .CSV</p>' +
-
-            '<form action="/uploader" method="post" enctype="multipart/form-data">' +
+        reply('<h1>GSA GENERIC API</h1><h2>Add a .CSV</h2>' +
+            '<form action="/upload" method="post" enctype="multipart/form-data">' +
             '<label for="name">Name</label><input name="name" type="text">' +
-            '<label for="file">Filename:</label>' +
+            '<label for="file">File:</label>' +
             '<input type="file" name="file" id="file"><br>' +
             '<input type="submit" name="submit" value="Submit">' +
             '</form>');
     }
-})
+});
 
 server.start();
 
@@ -247,3 +320,11 @@ Object.size = function(obj) {
     }
     return size;
 };
+
+function hasValue(obj, key, value) {
+    return obj.hasOwnProperty(key) && obj[key] === value;
+}
+
+function metaRefresh(delay, path) {
+    return '<META http-equiv="refresh" content="' + delay + ';URL=' + path + '">';
+}
